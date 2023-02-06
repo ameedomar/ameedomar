b964cb88fd87a89713ed84b6e80634c94f10ff3d
Ameed Omar <ameed_201010@hotmail.com>
2023-02-06 15:51:21 +0200

    third commit

--- src/PortfolioContainer/Footer/Footer.js
+++ src/PortfolioContainer/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from "react";
-import bg from "../../assets/Home/shape-bg22.png";
+import bg_white_reverse from "../../assets/Home/shape-bgr.png";
+import bg from "../../assets/Home/bg-half.jpg";
 import "../Footer/Footer.css";
 
 function Footer(props) {
   return (
-    <div className="footer">
-      <img src={bg} alt="no-footer" className="footer-img"></img>
+    <div className="footer-page-container">
+      <div>
+        <img src={bg} alt="no-footer" className="footer-bg"></img>
+      </div>
+      <div className="footer-revers">
+        <img
+          src={bg_white_reverse}
+          alt="no-footer"
+          className="footer-reverse"
+        ></img>
+      </div>
     </div>
   );
 }
